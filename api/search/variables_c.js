a27163ba788bd312ee554f258c47d32cfa6eab68
ayushman1210 <138183570+ayushman1210@users.noreply.github.com>
2026-02-14 12:30:46 +0000

    Deploying to gh-pages from @ ayushman1210/sipnet@8f489c5489ebbadae01f8e6ba5e1b77bab1dd851 🚀

---
+++ api/search/variables_c.js
@@ -0,0 +1,21 @@
+var searchData=
+[
+  ['name_0',['name',['../dd/df9/struct_single_output_item_struct.html#aa41c180b0c9900f0eab87e78598faf0a',1,'SingleOutputItemStruct::name'],['../df/d3a/struct_one_model_param_struct.html#a80c78b291ebc06ac9ccb58c6814e0f0e',1,'OneModelParamStruct::name']]],
+  ['nee_1',['nee',['../d3/d1f/struct_tracker_vars.html#a8b32589de454079b7d9e578f737373e7',1,'TrackerVars']]],
+  ['next_2',['next',['../de/dfa/struct_u_t__hash__handle.html#a93bc88ffe97f85ea0d9e0056b7118942',1,'UT_hash_handle']]],
+  ['nextclim_3',['nextClim',['../d4/d5e/struct_climate_vars.html#ab542473c23dec94db4eb707e33a1e1df',1,'ClimateVars']]],
+  ['nextevent_4',['nextEvent',['../d6/d43/struct_event_node.html#a5f73b9d29f86c2f634273d18a08c947a',1,'EventNode']]],
+  ['nextitem_5',['nextItem',['../dd/df9/struct_single_output_item_struct.html#a4e7cab99e2d9f9c460dbc6af7e0962c6',1,'SingleOutputItemStruct']]],
+  ['nitrogencycle_6',['nitrogenCycle',['../de/d66/struct_context.html#a3d65050b05d6ab2d8a444048af33cee1',1,'Context']]],
+  ['nleaching_7',['nLeaching',['../da/d95/struct_flux_vars.html#ae6894bdb22d16be538d35fe0f40a00b1',1,'FluxVars']]],
+  ['nleachingfrac_8',['nLeachingFrac',['../d0/dbf/struct_parameters.html#a9ab29748e391ed4cdfed28a35dceda71',1,'Parameters']]],
+  ['noexpand_9',['noexpand',['../db/d32/struct_u_t__hash__table.html#a635661789933752e7b83dac84430eae1',1,'UT_hash_table']]],
+  ['nonideal_5fitems_10',['nonideal_items',['../db/d32/struct_u_t__hash__table.html#a8cb66cfb259a204cda59a815e4db664f',1,'UT_hash_table']]],
+  ['npp_11',['npp',['../d3/d1f/struct_tracker_vars.html#a6391c6b6a5f95d40c7a42b577cae1c41',1,'TrackerVars']]],
+  ['num_5fbuckets_12',['num_buckets',['../db/d32/struct_u_t__hash__table.html#a3ed04b6233facaedf910672578d86339',1,'UT_hash_table']]],
+  ['num_5fitems_13',['num_items',['../db/d32/struct_u_t__hash__table.html#a74534cc14f080c96f94d8f5da83d9d76',1,'UT_hash_table']]],
+  ['numparams_14',['numParams',['../d8/dae/struct_model_params_struct.html#afaa276662aebf01c9ed5abca07956cda',1,'ModelParamsStruct']]],
+  ['numparamsread_15',['numParamsRead',['../d8/dae/struct_model_params_struct.html#a685a4cacf7bdf46a0e1d91b6f5cca613',1,'ModelParamsStruct']]],
+  ['nvolatilization_16',['nVolatilization',['../da/d95/struct_flux_vars.html#adf53dc00b1641e475cbfa6cbaf4829e1',1,'FluxVars']]],
+  ['nvolatilizationfrac_17',['nVolatilizationFrac',['../d0/dbf/struct_parameters.html#af8077952ecfe087243ac68460fa9c061',1,'Parameters']]]
+];
